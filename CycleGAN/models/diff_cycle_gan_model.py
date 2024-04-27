f197060aaf5e9fe7d15f5a70582f2135361f4cc8
hdhduxhd <101924605+hdhduxhd@users.noreply.github.com>
2024-04-27 13:17:09 +0800

    Update diff_cycle_gan_model.py

--- CycleGAN/models/diff_cycle_gan_model.py
+++ CycleGAN/models/diff_cycle_gan_model.py
@@ -60,7 +60,8 @@ class DiffCycleGANModel(BaseModel):
         """
         BaseModel.__init__(self, opt)
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
-        self.loss_names = ['D_A', 'G_A', 'cycle_A', 'idt_A', 'D_B', 'G_B', 'cycle_B', 'idt_B', 'diff']
+        # self.loss_names = ['D_A', 'G_A', 'cycle_A', 'idt_A', 'D_B', 'G_B', 'cycle_B', 'idt_B', 'diff']
+        self.loss_names = ['D_A', 'G_A', 'cycle_A', 'idt_A', 'D_B', 'G_B', 'cycle_B', 'idt_B']
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
         visual_names_A = ['real_A', 'fake_B', 'rec_A', 'real_A_noise', 'fake_B_noise']
         visual_names_B = ['real_B', 'fake_A', 'rec_B', 'real_B_noise', 'fake_A_noise']
@@ -71,20 +72,21 @@ class DiffCycleGANModel(BaseModel):
         self.visual_names = visual_names_A + visual_names_B  # combine visualizations for A and B
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>.
         if self.isTrain:
-            self.model_names = ['G_A', 'G_B', 'D_A', 'D_B', 'Denoise_A', 'Denoise_B']
+            # self.model_names = ['G_A', 'G_B', 'D_A', 'D_B', 'Denoise_A', 'Denoise_B']
+            self.model_names = ['G_A', 'G_B', 'D_A', 'D_B']
         else:  # during test time, only load Gs
-            self.model_names = ['G_A', 'G_B', 'Denoise_A', 'Denoise_B']
+            self.model_names = ['G_A', 'G_B']
 
         # define networks (both Generators and discriminators)
         # The naming is different from those used in the paper.
         # Code (vs. paper): G_A (G), G_B (F), D_A (D_Y), D_B (D_X)
-        self.netG_A = networks.define_G(opt.input_nc*2, opt.output_nc, opt.ngf, opt.netG, opt.norm,
+        self.netG_A = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm,
                                         not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
-        self.netG_B = networks.define_G(opt.output_nc*2, opt.input_nc, opt.ngf, opt.netG, opt.norm,
+        self.netG_B = networks.define_G(opt.output_nc, opt.input_nc, opt.ngf, opt.netG, opt.norm,
                                         not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
         self.diffusion = GaussianDiffusion()
-        self.netDenoise_A = UNet(in_channel=opt.input_nc, out_channel=opt.input_nc).to(self.device)
-        self.netDenoise_B = UNet(in_channel=opt.output_nc, out_channel=opt.output_nc).to(self.device)
+        # self.netDenoise_A = UNet(in_channel=opt.input_nc, out_channel=opt.input_nc).to(self.device)
+        # self.netDenoise_B = UNet(in_channel=opt.output_nc, out_channel=opt.output_nc).to(self.device)
 
         if self.isTrain:  # define discriminators
             self.netD_A = networks.define_D(opt.output_nc, opt.ndf, opt.netD,
@@ -102,7 +104,8 @@ class DiffCycleGANModel(BaseModel):
             self.criterionCycle = torch.nn.L1Loss()
             self.criterionIdt = torch.nn.L1Loss()
             # initialize optimizers; schedulers will be automatically created by function <BaseModel.setup>.
-            self.optimizer_G = torch.optim.Adam(itertools.chain(self.netG_A.parameters(), self.netG_B.parameters(), self.netDenoise_A.parameters(), self.netDenoise_B.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
+            # self.optimizer_G = torch.optim.Adam(itertools.chain(self.netG_A.parameters(), self.netG_B.parameters(), self.netDenoise_A.parameters(), self.netDenoise_B.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_G = torch.optim.Adam(itertools.chain(self.netG_A.parameters(), self.netG_B.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
             self.optimizer_D = torch.optim.Adam(itertools.chain(self.netD_A.parameters(), self.netD_B.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
             self.optimizers.append(self.optimizer_G)
             self.optimizers.append(self.optimizer_D)
@@ -122,79 +125,84 @@ class DiffCycleGANModel(BaseModel):
     
     def forward(self):
         batch_size = self.real_A.shape[0]
-        self.t = torch.randint(0, 2000, (batch_size,), device=self.device).long()
+        self.t = torch.randint(600, 700, (batch_size,), device=self.device).long()
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         self.noise_real_A = torch.randn_like(self.real_A)
         self.real_A_noise = self.diffusion.q_sample(self.real_A, self.t, noise=self.noise_real_A)
-        self.real_A_latent = self.netDenoise_A(self.real_A_noise, self.t)
-        self.fake_B = self.netG_A(torch.cat([self.real_A_noise, self.real_A_latent], dim=1))  # G_A(A)
+        # self.real_A_latent = self.netDenoise_A(self.real_A_noise, self.t)
+        # self.fake_B = self.netG_A(torch.cat([self.real_A_noise, self.real_A_latent], dim=1))  # G_A(A)
+        self.fake_B = self.netG_A(self.real_A_noise)  # G_A(A)
         self.noise_fake_B = torch.randn_like(self.fake_B)
         self.fake_B_noise = self.diffusion.q_sample(self.fake_B, self.t, noise=self.noise_fake_B)
-        self.fake_B_latent = self.netDenoise_B(self.fake_B_noise, self.t)
-        self.rec_A = self.netG_B(torch.cat([self.fake_B_noise, self.fake_B_latent], dim=1))   # G_B(G_A(A))
+        # self.fake_B_latent = self.netDenoise_B(self.fake_B_noise, self.t)
+        # self.rec_A = self.netG_B(torch.cat([self.fake_B_noise, self.fake_B_latent], dim=1))   # G_B(G_A(A))
+        self.rec_A = self.netG_B(self.fake_B_noise)   # G_B(G_A(A))
         self.noise_real_B = torch.randn_like(self.real_B)
         self.real_B_noise = self.diffusion.q_sample(self.real_B, self.t, noise=self.noise_real_B)
-        self.real_B_latent = self.netDenoise_B(self.real_B_noise, self.t)
-        self.fake_A = self.netG_B(torch.cat([self.real_B_noise, self.real_B_latent], dim=1))  # G_B(B)
+        # self.real_B_latent = self.netDenoise_B(self.real_B_noise, self.t)
+        # self.fake_A = self.netG_B(torch.cat([self.real_B_noise, self.real_B_latent], dim=1))  # G_B(B)
+        self.fake_A = self.netG_B(self.real_B_noise)  # G_B(B)
         self.noise_fake_A = torch.randn_like(self.fake_A)
         self.fake_A_noise = self.diffusion.q_sample(self.fake_A, self.t, noise=self.noise_fake_A)
-        self.fake_A_latent = self.netDenoise_A(self.fake_A_noise, self.t)
-        self.rec_B = self.netG_A(torch.cat([self.fake_A_noise, self.fake_A_latent], dim=1))   # G_A(G_B(B))
+        # self.fake_A_latent = self.netDenoise_A(self.fake_A_noise, self.t)
+        # self.rec_B = self.netG_A(torch.cat([self.fake_A_noise, self.fake_A_latent], dim=1))   # G_A(G_B(B))
+        self.rec_B = self.netG_A(self.fake_A_noise)   # G_A(G_B(B))
 
     def get_output_B(self, input, type1='one', type2='one'):
-        t = random.randint(200, 500)
+        # t = random.randint(200, 500)
+        batch_size = input.shape[0]
+        t = torch.randint(600, 700, (batch_size,), device=self.device).long()
         noise_input = torch.randn_like(input)
-        input_noise = self.diffusion.q_sample(input, torch.full((input.shape[0],), t, device=self.device, dtype=torch.long), noise=noise_input)
-        input_latent = self.netDenoise_B(input_noise, torch.full((input.shape[0],), t, device=self.device, dtype=torch.long))
+        input_noise = self.diffusion.q_sample(input, t, noise=noise_input)
+        # input_latent = self.netDenoise_B(input_noise, torch.full((input.shape[0],), t, device=self.device, dtype=torch.long))
         if type1 == 'one':
-            output1 = self.netG_B(torch.cat([input_noise, input_latent], dim=1))  # denoise one step
+            output1 = self.netG_B(input_noise)  # denoise one step
         else:
             output1 = self.diffusion.sample(self.netDenoise_B, img=input_noise, t=t)[-1] #denoise step by step
             output1 = torch.from_numpy(output1).to(self.device)
         
         noise_output1 = torch.randn_like(output1)
-        output1_noise = self.diffusion.q_sample(output1, torch.full((input.shape[0],), t, device=self.device, dtype=torch.long), noise=noise_output1)
-        output1_latent = self.netDenoise_A(output1_noise, torch.full((input.shape[0],), t, device=self.device, dtype=torch.long))
+        output1_noise = self.diffusion.q_sample(output1, t, noise=noise_output1)
+        # output1_latent = self.netDenoise_A(output1_noise, torch.full((input.shape[0],), t, device=self.device, dtype=torch.long))
         if type2 == 'one':
-            output2 = self.netG_A(torch.cat([output1_noise, output1_latent], dim=1))
+            output2 = self.netG_A(output1_noise)
         else:
             output2 = self.diffusion.sample(self.netDenoise_A, img=output1_noise, t=t)[-1]
             output2 = torch.from_numpy(output2).to(self.device)
         
         return output1, output2  #refine, recon
 
-    def compute_visuals(self):
-        self.visual_names.extend(['fake_B_sample', 'rec_A_sample', 'fake_A_sample', 'rec_B_sample'])
-        t = random.randint(200, 500)
-        batch_size = self.real_A.shape[0]
-        self.noise_real_A = torch.randn_like(self.real_A)
-        self.real_A_noise = self.diffusion.q_sample(self.real_A, torch.full((batch_size,), t, device=self.device, dtype=torch.long), noise=self.noise_real_A)
-        self.real_A_latent = self.netDenoise_A(self.real_A_noise, torch.full((batch_size,), t, device=self.device, dtype=torch.long))
-        self.fake_B = self.netG_A(torch.cat([self.real_A_noise, self.real_A_latent], dim=1))  # G_A(A)
-        self.fake_B_sample = self.diffusion.sample(self.netDenoise_A, img=self.real_A_noise, t=t)[-1] #denoise step by step
-        self.fake_B_sample = torch.from_numpy(self.fake_B_sample).to(self.device)
+    # def compute_visuals(self):
+    #     self.visual_names.extend(['fake_B_sample', 'rec_A_sample', 'fake_A_sample', 'rec_B_sample'])
+    #     t = random.randint(200, 500)
+    #     batch_size = self.real_A.shape[0]
+    #     self.noise_real_A = torch.randn_like(self.real_A)
+    #     self.real_A_noise = self.diffusion.q_sample(self.real_A, torch.full((batch_size,), t, device=self.device, dtype=torch.long), noise=self.noise_real_A)
+    #     self.real_A_latent = self.netDenoise_A(self.real_A_noise, torch.full((batch_size,), t, device=self.device, dtype=torch.long))
+    #     self.fake_B = self.netG_A(torch.cat([self.real_A_noise, self.real_A_latent], dim=1))  # G_A(A)
+    #     self.fake_B_sample = self.diffusion.sample(self.netDenoise_A, img=self.real_A_noise, t=t)[-1] #denoise step by step
+    #     self.fake_B_sample = torch.from_numpy(self.fake_B_sample).to(self.device)
         
-        self.noise_fake_B = torch.randn_like(self.fake_B)
-        self.fake_B_noise = self.diffusion.q_sample(self.fake_B, torch.full((batch_size,), t, device=self.device, dtype=torch.long), noise=self.noise_fake_B)
-        self.fake_B_latent = self.netDenoise_B(self.fake_B_noise, torch.full((batch_size,), t, device=self.device, dtype=torch.long))
-        self.rec_A = self.netG_B(torch.cat([self.fake_B_noise, self.fake_B_latent], dim=1))   # G_B(G_A(A))
-        self.rec_A_sample = self.diffusion.sample(self.netDenoise_B, img=self.fake_B_noise, t=t)[-1] #denoise step by step
-        self.rec_A_sample = torch.from_numpy(self.rec_A_sample).to(self.device)
+    #     self.noise_fake_B = torch.randn_like(self.fake_B)
+    #     self.fake_B_noise = self.diffusion.q_sample(self.fake_B, torch.full((batch_size,), t, device=self.device, dtype=torch.long), noise=self.noise_fake_B)
+    #     self.fake_B_latent = self.netDenoise_B(self.fake_B_noise, torch.full((batch_size,), t, device=self.device, dtype=torch.long))
+    #     self.rec_A = self.netG_B(torch.cat([self.fake_B_noise, self.fake_B_latent], dim=1))   # G_B(G_A(A))
+    #     self.rec_A_sample = self.diffusion.sample(self.netDenoise_B, img=self.fake_B_noise, t=t)[-1] #denoise step by step
+    #     self.rec_A_sample = torch.from_numpy(self.rec_A_sample).to(self.device)
         
-        self.noise_real_B = torch.randn_like(self.real_B)
-        self.real_B_noise = self.diffusion.q_sample(self.real_B, torch.full((batch_size,), t, device=self.device, dtype=torch.long), noise=self.noise_real_B)
-        self.real_B_latent = self.netDenoise_B(self.real_B_noise, torch.full((batch_size,), t, device=self.device, dtype=torch.long))
-        self.fake_A = self.netG_B(torch.cat([self.real_B_noise, self.real_B_latent], dim=1))  # G_B(B)
-        self.fake_A_sample = self.diffusion.sample(self.netDenoise_B, img=self.real_B_noise, t=t)[-1] #denoise step by step
-        self.fake_A_sample = torch.from_numpy(self.fake_A_sample).to(self.device)
+    #     self.noise_real_B = torch.randn_like(self.real_B)
+    #     self.real_B_noise = self.diffusion.q_sample(self.real_B, torch.full((batch_size,), t, device=self.device, dtype=torch.long), noise=self.noise_real_B)
+    #     self.real_B_latent = self.netDenoise_B(self.real_B_noise, torch.full((batch_size,), t, device=self.device, dtype=torch.long))
+    #     self.fake_A = self.netG_B(torch.cat([self.real_B_noise, self.real_B_latent], dim=1))  # G_B(B)
+    #     self.fake_A_sample = self.diffusion.sample(self.netDenoise_B, img=self.real_B_noise, t=t)[-1] #denoise step by step
+    #     self.fake_A_sample = torch.from_numpy(self.fake_A_sample).to(self.device)
         
-        self.noise_fake_A = torch.randn_like(self.fake_A)
-        self.fake_A_noise = self.diffusion.q_sample(self.fake_A, torch.full((batch_size,), t, device=self.device, dtype=torch.long), noise=self.noise_fake_A)
-        self.fake_A_latent = self.netDenoise_A(self.fake_A_noise, torch.full((batch_size,), t, device=self.device, dtype=torch.long))
-        self.rec_B = self.netG_A(torch.cat([self.fake_A_noise, self.fake_A_latent], dim=1))   # G_A(G_B(B))
-        self.rec_B_sample = self.diffusion.sample(self.netDenoise_A, img=self.fake_A_noise, t=t)[-1] #denoise step by step
-        self.rec_B_sample = torch.from_numpy(self.rec_B_sample).to(self.device)
-        
+    #     self.noise_fake_A = torch.randn_like(self.fake_A)
+    #     self.fake_A_noise = self.diffusion.q_sample(self.fake_A, torch.full((batch_size,), t, device=self.device, dtype=torch.long), noise=self.noise_fake_A)
+    #     self.fake_A_latent = self.netDenoise_A(self.fake_A_noise, torch.full((batch_size,), t, device=self.device, dtype=torch.long))
+    #     self.rec_B = self.netG_A(torch.cat([self.fake_A_noise, self.fake_A_latent], dim=1))   # G_A(G_B(B))
+    #     self.rec_B_sample = self.diffusion.sample(self.netDenoise_A, img=self.fake_A_noise, t=t)[-1] #denoise step by step
+    #     self.rec_B_sample = torch.from_numpy(self.rec_B_sample).to(self.device)
 
     def backward_D_basic(self, netD, real, fake):
         """Calculate GAN loss for the discriminator
