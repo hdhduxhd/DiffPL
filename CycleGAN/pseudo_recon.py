aa0705bbff3a7e0831d59455f637f7f903a0dab9
hdhduxhd <101924605+hdhduxhd@users.noreply.github.com>
2024-05-07 14:37:54 +0800

    Update pseudo_recon.py

--- CycleGAN/pseudo_recon.py
+++ CycleGAN/pseudo_recon.py
@@ -64,8 +64,6 @@ if __name__ == '__main__':
     prob_dic = {}
 
     for i, sample in enumerate(domain_loaderT):
-        flag = False
-        visualizer.reset() 
         target_image, target_label, target_img_name = sample['image'], sample['map'], sample['img_name']
         target_pl = torch.stack([torch.from_numpy(refine_pseudo_label_dic.get(i)) for i in target_img_name])
         target_prob_pl = torch.stack([torch.from_numpy(refine_prob_dic.get(i)) for i in target_img_name])
