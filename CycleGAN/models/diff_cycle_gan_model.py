d0d4838653d5f020af7eba56f4864a0abd140cdd
hdhduxhd <101924605+hdhduxhd@users.noreply.github.com>
2024-05-10 17:54:55 +0800

    Update diff_cycle_gan_model.py

--- CycleGAN/models/diff_cycle_gan_model.py
+++ CycleGAN/models/diff_cycle_gan_model.py
@@ -352,14 +352,14 @@ class DiffCycleGANModel(BaseModel):
         # Identity loss
         if lambda_idt > 0:
             # G_A should be identity if real_B is fed: ||G_A(B) - B||
-            self.noise_real_B = torch.randn_like(self.real_B)
-            self.real_B_noise = self.diffusion.q_sample(self.real_B, self.t, noise=self.noise_real_B)
+            # self.noise_real_B = torch.randn_like(self.real_B)
+            # self.real_B_noise = self.diffusion.q_sample(self.real_B, self.t, noise=self.noise_real_B)
             # self.real_B_latent = self.netDenoise_B(self.real_B_noise, self.t)
             self.idt_A = self.netG_A(self.real_B_noise)
             self.loss_idt_A = self.criterionIdt(self.idt_A, self.real_B) * lambda_B * lambda_idt
             # G_B should be identity if real_A is fed: ||G_B(A) - A||
-            self.noise_real_A = torch.randn_like(self.real_A)
-            self.real_A_noise = self.diffusion.q_sample(self.real_A, self.t, noise=self.noise_real_A)
+            # self.noise_real_A = torch.randn_like(self.real_A)
+            # self.real_A_noise = self.diffusion.q_sample(self.real_A, self.t, noise=self.noise_real_A)
             # self.real_A_latent = self.netDenoise_A(self.real_A_noise, self.t)
             self.idt_B = self.netG_B(self.real_A_noise)
             self.loss_idt_B = self.criterionIdt(self.idt_B, self.real_A) * lambda_A * lambda_idt
